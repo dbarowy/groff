50425bdf2faa2530942f35b06eaa8ab3e094136c
dbarowy <dbarowy@users.noreply.github.com>
2015-10-20 10:19:23 -0400

    Add header for missing imports.

---
+++ src/barowymissing.h
@@ -0,0 +1,4 @@
+// src/roff/troff/input.cpp looks for these
+void process_input_stack(void);
+void end_diversions(void);
+void title(void);
--- src/roff/troff/input.cpp
+++ src/roff/troff/input.cpp
@@ -19,6 +19,8 @@ You should have received a copy of the GNU General Public License along
 with groff; see the file COPYING.  If not, write to the Free Software
 Foundation, 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. */
 
+#include "../../barowymissing.h"
+
 #include "troff.h"
 #include "dictionary.h"
 #include "hvunits.h"
